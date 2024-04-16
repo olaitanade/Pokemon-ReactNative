ac7e3873da4b3bd81f9583ac556f34ef0895f470
Adetayo Olaitan <olaitan.adetayo@gmail.com>
2024-04-16 03:35:48 +0100

    wip: adding comment and references

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -5,6 +5,7 @@ import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
 import {StateProvider} from 'core/state/store';
 import {GestureHandlerRootView} from 'react-native-gesture-handler';
 
+//setup react-query client
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
